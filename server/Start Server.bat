daf9fc4b81e9be6e3527dc8ec2578a4a0b21b33a
ibemad@gmail.com <ibemad@gmail.com@0b7b69cc-2dba-df56-e3eb-7f138e0a0778>
2014-02-25 19:45:09 +0000

    modified stuff

--- server/Start Server.bat
+++ server/Start Server.bat
@@ -1,1 +1,3 @@
-START srcds.exe -port 27015 +map lobby -game swarm -console
+DEL "%~dp0swarm\consolelog.log"
+
+START srcds.exe +con_logfile "consolelog.log" -port 27015 +map lobby -game swarm -console
